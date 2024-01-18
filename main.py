1225a76506d25d07cf2ad9ffda561d472c08d743
Matthew Morgan <149610460+MattMNIA@users.noreply.github.com>
2024-01-17 18:00:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import sudoku_loader as sl
 import sudoku_Solver as ss
 import easyocr
 import cv2
-import imutils
+import imutils  
+import os
 
-puzzle = "Sudoku_board.png"
-
+# for filename in os.listdir('Test Puzzles'):
+#     puzzle = os.path.join('Test Puzzles', filename)
+    
+puzzle = 'Test Puzzles\Sudoku_board.png'
 
 
 board = sl.findPuzzle(puzzle)
